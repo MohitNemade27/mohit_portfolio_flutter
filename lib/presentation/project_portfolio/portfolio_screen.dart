bcac2d9ea06c1ce7f2d8c84142d8a37132c664db
Mohit Nemade <mohit.nemade@wolterskluwer.com>
2025-05-10 20:11:19 +0530

    portfolio screen modified

--- lib/presentation/project_portfolio/portfolio_screen.dart
+++ lib/presentation/project_portfolio/portfolio_screen.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:mohit_portfolio_flutter_web/app/app_constant.dart';
 import 'package:mohit_portfolio_flutter_web/app/common_widgets/glass_box_decoration.dart';
 import 'package:mohit_portfolio_flutter_web/app/common_widgets/portfolio_dialoge/portfolio_dialoge.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PortfolioSection extends StatelessWidget {
   const PortfolioSection({super.key});
@@ -17,11 +19,12 @@ class PortfolioSection extends StatelessWidget {
           spacing: 20,
           runSpacing: 20,
           children: [
-            _projectCard(context, "Lippincott Solutions", "assets/lippincott.png", project1Details,"https://advisor.qa.lww.com/lna/login",'assets/lns_mobile.png'),
-            _projectCard(context, "Garbage Weight IOT", "assets/img_company_logo.png", project2Details,"NA",'assets/flutter_garbage_weight_management_img.png'),
-            _projectCard(context, "NMC Face Attendance", "assets/face.png", project3Details,"https://play.google.com/store/apps/details?id=com.swt.nmc_attendance&hl=en",'NA'),
-            _projectCard(context, "HSEF@Champion", "assets/snap@champ_img.png", project4Details,"https://play.google.com/store/apps/details?id=com.champmgt.snapchampion&hl=en",'NA'),
-            _projectCard(context, "Sunrayz", "assets/sunrayz.png", project5Details,"https://play.google.com/store/apps/details?id=com.sunrayztechnology.apex_school",'NA'),
+            _projectCard(context, "Lippincott Solutions (Mobile)", "assets/lippincott.png", project1Details, "", 'assets/lns_mobile.png'),
+            _projectCard(context, "Lippincott Solutions (web)", "assets/lippincott.png", project1Details, "https://advisor.qa.lww.com/lna/login", 'assets/lnsWeb.png'),
+            _projectCard(context, "Garbage Weight IOT", "assets/img_company_logo.png", project2Details, "NA", 'assets/flutter_garbage_weight_management_img.png'),
+            _projectCard(context, "NMC Face Attendance", "assets/face.png", project3Details, "https://play.google.com/store/apps/details?id=com.swt.nmc_attendance&hl=en", 'assets/nmc_face.png'),
+            _projectCard(context, "HSEF@Champion", "assets/snap@champ_img.png", project4Details, "https://play.google.com/store/apps/details?id=com.champmgt.snapchampion&hl=en", 'assets/snapChamp.png'),
+            _projectCard(context, "Sunrayz", "assets/sunrayz.png", project5Details, "https://play.google.com/store/apps/details?id=com.sunrayztechnology.apex_school", 'assets/sunrayz_mobile_demo.png'),
           ],
         )
       ],
@@ -36,18 +39,18 @@ class PortfolioSection extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-        Container(
-        height: 100,
-        width: 100,
-        decoration: getGlassCardDecoration(),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: Image.asset(
-            imagePath,
-            fit: BoxFit.scaleDown, // Or use BoxFit.contain if needed
+          Container(
+            height: 100,
+            width: 100,
+            decoration: getGlassCardDecoration(),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: CachedNetworkImage(
+                  imageUrl: "https://raw.githubusercontent.com/MohitNemade27/mohit_portfolio_flutter/main/$imagePath",
+                  placeholder: (context, url) => CircularProgressIndicator(), // Show while loading
+                  errorWidget: (context, url, error) => Icon(Icons.error), // Show if there's an error
+                )),
           ),
-        ),
-      ),
           const SizedBox(height: 12),
           Center(
             child: Text(
@@ -71,9 +74,7 @@ class PortfolioSection extends StatelessWidget {
                 ),
               ),
               onPressed: () {
-                 showDialog(
-                  context: context,
-                  builder: (_) => ProjectDetailDialog(title: title, details: details, playStoreLink: playStoreLink,appImagePath:appImagePath));
+                showDialog(context: context, builder: (_) => ProjectDetailDialog(title: title, details: details, playStoreLink: playStoreLink, appImagePath: appImagePath));
               },
               child: const Text('More detail'),
             ),
