33a499c2a94d4135c4e18b0144ea4c7a17430b5f
Mohit Nemade <mohit.nemade@wolterskluwer.com>
2025-05-14 22:14:22 +0530

    10 th  Deployment Flutter Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1621726033"
+    serviceWorkerVersion: "2122010631"
   }
 });
